f16fb5cff83a520a8857b6f3eb13bfa08a5a6aee
thezanke <thezanke@gmail.com>
2020-09-21 23:26:16 -0400

    make log level configurable

--- logger.ts
+++ logger.ts
@@ -1,8 +1,11 @@
 import * as log from "https://deno.land/std/log/mod.ts";
+import config from "./config.ts";
 
 await log.setup({
   handlers: {
-    console: new log.handlers.ConsoleHandler("DEBUG"),
+    console: new log.handlers.ConsoleHandler(
+      config.LOG_LEVEL as log.LevelName || "DEBUG",
+    ),
   },
   loggers: {
     default: { level: "DEBUG", handlers: ["console"] },
