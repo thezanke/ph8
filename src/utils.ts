7a7a498646e1e72af8ff244fae72545909982d54
Alex Howard <thezanke@gmail.com>
2021-01-26 22:10:22 -0500

    fix random

--- src/utils.ts
+++ src/utils.ts
@@ -4,5 +4,5 @@ export const pickRandom = (arr: any[]) =>
 export const randomInt = (min: number, max: number) => {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(max - min) + min;
+  return Math.floor(Math.random() * max - min) + min;
 };
