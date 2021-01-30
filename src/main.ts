80e1ec81bc75e378a62986c567ac8a175a0a33f5
Alex Howard <thezanke@gmail.com>
2021-01-29 19:06:10 -0500

    Updates for statefulset

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import { botID, startBot } from "https://deno.land/x/discordeno@10.1.0/mod.ts";
+import {
+  botID,
+  startBot,
+} from "https://deno.land/x/discordeno@10.1.0/mod.ts";
 import config from "./config.ts";
 import { handleCommands } from "./handle_commands.ts";
 import { handleDigression } from "./handle_digression.ts";
@@ -7,31 +10,37 @@ import { handleReactions } from "./handle_reactions.ts";
 import { handleReactionReplies } from "./handle_reaction_replies.ts";
 import { server } from "./http_server.ts";
 
-startBot({
-  token: config.DISCORD_BOT_TOKEN,
-  intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"],
-  eventHandlers: {
-    ready() {
-      console.log("Successfully connected to gateway!");
-      console.log(`-> botID: ${botID}`);
-    },
-    messageCreate(message) {
-      handleCommands(message) ||
-        handleReactionReplies(message) ||
-        handleDigression(message) ||
-        handlePoorSources(message);
-    },
-    reactionAdd(...args) {
-      const [, emoji] = args;
-      if (!emoji.name) return;
-      handleReactions(...args);
-    },
-    reactionRemove(...args) {
-      const [, emoji] = args;
-      if (!emoji.name) return;
-      handleReactions(...args, true);
-    },
-  },
-});
-
-server.listen({ port: config.HTTP_PORT });
+try {
+  await Promise.all([
+    startBot({
+      token: config.DISCORD_BOT_TOKEN,
+      intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"],
+      eventHandlers: {
+        ready() {
+          console.log("Successfully connected to gateway!");
+          console.log(`-> botID: ${botID}`);
+        },
+        messageCreate(message) {
+          handleCommands(message) ||
+            handleReactionReplies(message) ||
+            handleDigression(message) ||
+            handlePoorSources(message);
+        },
+        reactionAdd(...args) {
+          const [, emoji] = args;
+          if (!emoji.name) return;
+          handleReactions(...args);
+        },
+        reactionRemove(...args) {
+          const [, emoji] = args;
+          if (!emoji.name) return;
+          handleReactions(...args, true);
+        },
+      },
+    }),
+    server.listen({ port: config.HTTP_PORT }),
+  ]);
+} catch (e) {
+  console.error(e);
+  Deno.exit(-1);
+}
