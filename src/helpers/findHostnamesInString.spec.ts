45652ee5dffc5a67c00442e679f0cafaa1ffc173
Alex Howard <thezanke@gmail.com>
2021-09-05 08:53:33 -0400

    update regex

--- src/helpers/findHostnamesInString.spec.ts
+++ src/helpers/findHostnamesInString.spec.ts
@@ -4,9 +4,9 @@ describe('findHostnamesInString', () => {
   it('Returns correct domain names', () => {
     expect(
       findHostnamesInString(
-        `https://www.example.com is cool but https://bad-website.com is dangerous.
-        Look at this picture https://bad-website.com/images/bad-image.jpg`,
+        `https://www.example.com is cool but https://bad-website.co.uk is dangerous.
+        Look at this picture https://bad-website.co.uk/images/bad-image.jpg`,
       ),
-    ).toEqual(['www.example.com', 'bad-website.com', 'bad-image.jpg']);
+    ).toEqual(['www.example.com', 'bad-website.co.uk', 'bad-image.jpg']);
   });
 });
